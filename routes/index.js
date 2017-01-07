7222788b18385a13b7fcace1b7ff5cfc74018037
Erick <ericksaurisyl@gmail.com>
2017-01-06 22:21:42 -0500

    lol2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
+const projects = [
+      {
+        name: 'twosevenzero',
+        title: '270',
+        category: 'Ionic Framework',
+        link: '/270',
+        description: 'Race to the White House',
+        image: './img/270.png'
+      },
+      {
+        name: 'clickergame',
+        title: 'Clicker Game',
+        link: '/clickergame',
+        category: 'Javascript',
+        description: 'Save Bob!',
+        image: './img/clickergame.png'
+      },
+      {
+        name: 'nest',
+        title: 'Nest',
+        link: '/nest',
+        category: 'Javascript',
+        description: 'Join the flock',
+        image: './img/nestchat.png'
+      },
+      {
+        name: 'admitone',
+        title: 'Admit One ',
+        link: '/admitone',
+        category: 'Illustrator',
+        description: 'Find out what\'s happening',
+        image: './img/admitone.jpg'
+      },
+      {
+        name: 'audiovisualizer',
+        title: 'Audio Visualizer',
+        link: '/audiovisualizer',
+        category: 'Javascript',
+        description: 'Visualize audio',
+        image: './img/audiovisualizer.png'
+      }
+    ];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Erick Sauri' });
+  var model = {
+    title: 'Erick Sauri',
+    project: projects
+  };
+
+  res.render('index', model);
 });
 
 module.exports = router;
